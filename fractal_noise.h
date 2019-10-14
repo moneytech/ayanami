060b22b8c4ec6c918eb67931e2f948daea2ab0b7
nicebyte@mailinator.com <nicebyte@gpfault.net>
2019-10-14 01:36:47 -0700

    Add 3-octave fractal noise

---
+++ fractal_noise.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "perlin.h"
+
+class fractal_noise : public noise {
+public:
+  fractal_noise(const perlin_noise *oc1,
+                const perlin_noise *oc2,
+                const perlin_noise *oc3) : octaves_(oc1, oc2, oc3) {}
+
+  float sample(const nm::float3 &p) const override {
+    return octaves_[0]->sample(p) +
+           octaves_[1]->sample(p) +
+           octaves_[2]->sample(p);
+  }
+
+private:
+  const perlin_noise *octaves_[3];
+};
