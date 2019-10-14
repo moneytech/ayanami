6afded0e98f7f1c3786a501295d4231c07f47769
nicebyte@mailinator.com <nicebyte@gpfault.net>
2019-10-14 01:58:22 -0700

    Fix fractal_noise compilation

--- fractal_noise.h
+++ fractal_noise.h
@@ -6,7 +6,7 @@ class fractal_noise : public noise {
 public:
   fractal_noise(const perlin_noise *oc1,
                 const perlin_noise *oc2,
-                const perlin_noise *oc3) : octaves_(oc1, oc2, oc3) {}
+                const perlin_noise *oc3) : octaves_ { oc1, oc2, oc3 } {}
 
   float sample(const nm::float3 &p) const override {
     return octaves_[0]->sample(p) +
